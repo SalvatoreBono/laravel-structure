d32194e4c199555af4c21e2f128e4be621b40463
SalvatoreBono <salvatore.bono2001@libero.it>
2023-10-04 11:24:59 +0200

    fix view, web and controller

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, "index"])->name("index");
+Route::get('/', [HomeController::class, "index"])->name("home");
 
 /* Route::resource("names", NameController::class); */
 
